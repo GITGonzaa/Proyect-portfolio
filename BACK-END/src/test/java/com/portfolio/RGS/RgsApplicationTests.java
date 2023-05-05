65cb6eb137db4ab980d5d1d32bf3c0d90ebf336a
GITGonzaa <gonzaloandroid8@gmail.com>
2023-05-04 22:10:38 -0300

    cree carpeta back

---
+++ BACK-END/src/test/java/com/portfolio/RGS/RgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.RGS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
